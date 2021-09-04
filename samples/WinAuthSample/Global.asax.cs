ed6022e3253991043902af097e2930a5bb297067
Alfus Inigo Jaganathan <alfusinigo.j@gmail.com>
2021-09-04 01:27:36 -0400

    wip - upgrading sample

---
+++ samples/WinAuthSample/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using PivotalServices.AspNet.Auth.Extensions;
+using PivotalServices.AspNet.Bootstrap.Extensions;
+
+namespace WinAuthSample
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AppBuilder.Instance
+                .AddWindowsAuthentication()
+                .Build()
+                .Start();
+        }
+
+        protected void Application_Stop()
+        {
+            AppBuilder.Instance.Stop();
+        }
+    }
+}
